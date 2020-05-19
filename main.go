a398e146a5967497c5048aa2cee5bc773642b0cb
longfangsong <longfangsong@icloud.com>
2020-05-19 22:06:39 +0800

    interface for fetching all

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	http.HandleFunc("/ping", handler.PingPongHandler)
 	http.HandleFunc("/college", handler.CollegeHandler)
+	http.HandleFunc("/colleges", handler.CollegesHandler)
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
